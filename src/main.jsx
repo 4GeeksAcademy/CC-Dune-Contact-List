0252bea70b0c0390a0079f09e66102a172d95f79
Clay Beach <wcbeach30@gmail.com>
2025-06-15 21:20:39 +0000

    refactored main.jsx for the proper components to display contact form and list

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'  // Global styles for your application
-import { RouterProvider } from "react-router-dom";  // Import RouterProvider to use the router
-import { router } from "./routes";  // Import the router configuration
-import { StoreProvider } from './hooks/useGlobalReducer';  // Import the StoreProvider for global state management
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import RouterProvider to use the router
+import { ContactList } from "./pages/ContactList";
+import { ContactForm } from ".pages/ContactForm";
+import { ContactsProvider } from ".context/ContactsContext";
+
+import { StoreProvider } from "./hooks/useGlobalReducer"; // Import the StoreProvider for global state management
 
 const Main = () => {
-    return (
-        <React.StrictMode>  
-            {/* Provide global state to all components */}
-            <StoreProvider> 
-                {/* Set up routing for the application */} 
-                <RouterProvider router={router}>
-                </RouterProvider>
-            </StoreProvider>
-        </React.StrictMode>
-    );
-}
+  return (
+    <React.StrictMode>
+      {/* Provide global state to all components */}
+      <StoreProvider>
+        {/* Set up routing for the application */}
+        <RouterProvider router={router}></RouterProvider>
+      </StoreProvider>
+    </React.StrictMode>
+  );
+};
 
 // Render the Main component into the root DOM element.
-ReactDOM.createRoot(document.getElementById('root')).render(<Main />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ContactsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ContactList />} />
+          <Route path="/new" element={<ContactForm />} />
+          <Route path="/edit/:id" element={<ContactForm />} />
+        </Routes>
+      </BrowserRouter>
+    </ContactsProvider>
+  </React.StrictMode>
+);
